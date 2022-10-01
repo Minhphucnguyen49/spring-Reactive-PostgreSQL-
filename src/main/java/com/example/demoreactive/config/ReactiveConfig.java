2c0c29cedd7e90971e623dbfcc7944b8d42a4a8a
stmunguy <Minh.P.b.Nguyen@stud.h-da.de>
2022-10-01 17:12:24 +0200

    first draft

---
+++ src/main/java/com/example/demoreactive/config/ReactiveConfig.java
@@ -0,0 +1,14 @@
+package com.example.demoreactive.config;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ReactiveConfig {
+
+
+
+
+
+
+}
---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,6 @@
+CREAT TABLE public.plane
+    (
+        id BIGSERIAL PRIMARY KEY,
+        registration_number VARCHAR(255),
+        model_type VARCHAR(255)
+    );
